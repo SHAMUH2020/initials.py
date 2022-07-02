9672e68f14fe82ab0a43c61b354dbfc2b922559c
Shanthamurthy Hanumantharayappa <shantha_mh@optumservetech.com>
2022-07-02 17:26:14 -0400

    Update shipping.py
    What is the cheapest method of shipping a 4.8 pound package and how much would it cost?
    
    What is the cheapest method of shipping a 41.5 pound package and how much would it cost?
    
    (See hint for answers)

--- Objectives/Sal's Shipping/shipping.py
+++ Objectives/Sal's Shipping/shipping.py
@@ -1,1 +1,34 @@
-starting....
+def shipping_cost_ground(weight):
+
+# Ground Shipping
+  if weight <= 2:
+    price_per_pound = 1.5
+  elif weight <= 6:
+    price_per_pound = 3.00
+  elif weight <= 10:
+    price_per_pound = 4.00
+  else:
+    price_per_pound =  4.75
+
+  return 20 + (price_per_pound * weight)
+
+print ("Ground Shipping: $", shipping_cost_ground(4.8))
+
+# Ground Shipping Premium
+shipping_cost_ground_premium = 125.00
+print("Ground Shipping Premium: $", shipping_cost_ground_premium)
+
+# Drone Shipping
+
+def shipping_cost_drone(weight):
+  if weight <= 2:
+    price_per_pound = 4.50
+  elif weight <= 6:
+    price_per_pound = 9.00
+  elif weight <= 10:
+    price_per_pound = 12.00
+  else:
+    price_per_pound =  14.25
+
+  return price_per_pound * weight
+print ("Drone Shipping: $", shipping_cost_drone (4.8))
