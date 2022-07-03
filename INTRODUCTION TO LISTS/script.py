2764808381d091ddd837e58aad1344a22af7a007
Shanthamurthy Hanumantharayappa <shantha_mh@optumservetech.com>
2022-07-03 14:24:13 -0400

    Update script.py
    In progress...

--- INTRODUCTION TO LISTS/script.py
+++ INTRODUCTION TO LISTS/script.py
@@ -1,3 +1,16 @@
+""" What is a List?
+In programming, it is common to want to work with collections of data. In Python, a list is one of the many built-
+in data structures that allows us to work with a collection of data in sequential order.
+
+Remove the # in front of the definition of the list broken_heights. If you run this code, you’ll get an error in your terminal:
+
+SyntaxError: invalid syntax
+Add commas (,) to broken_heights so that it runs without errors."""
+
 heights = [61, 70, 67, 64, 65]
 
+# broken_heights = [65 71 59 62]
+
+# The correct line of code is:
 broken_heights = [65, 71, 59, 62]
+
